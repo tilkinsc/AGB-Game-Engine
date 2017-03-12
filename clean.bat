18bb3a78be7e3cc52761c970e8473c8a0617b0be
bud <bud@BudPc>
2017-03-12 15:44:05 -0400

    Added bat file to deal with trash compilation files

---
+++ clean.bat
@@ -0,0 +1,28 @@
+@echo off
+TITLE Compiling and Making ELF...
+
+setlocal
+	
+	echo Deleting possible extra objects
+	
+	del /Q src\*.o
+	del /Q src\gfx\*.o
+	del /Q src\res\background\*.o
+	del /Q src\res\bitmap\*.o
+	del /Q src\res\sprite\*.o
+	del /Q src\res\font\*.o
+	del /Q src\res\map\*.o
+	del /Q src\res\sprite\*.o
+	
+	echo Deleting trash test compilation objects
+	
+	del /Q obj\*.o
+	
+	echo Deleting trash test binaries
+	
+	del /Q bin\*.elf
+	del /Q bin\*.gba
+	
+endlocal
+
+echo Done.
